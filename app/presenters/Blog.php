d70b986aad34f11e0d9a09270b4a814eac39f756
Ondrej Bedajanek <bedajanek.o@gmail.com>
2015-09-13 08:48:45 +0200

    Exception on Blog

--- app/presenters/Blog.php
+++ app/presenters/Blog.php
@@ -7,6 +7,8 @@ use Kdyby\Doctrine\EntityManager;
 use Nette;
 use App\Model;
 use App\Article;
+use Nette\Application\ApplicationException;
+use Tracy\Debugger;
 
 
 class BlogPresenter extends BasePresenter
@@ -33,12 +35,12 @@ class BlogPresenter extends BasePresenter
 //		$dao = $this->articles;
 		$this->template->articles = $this->articles->getArticles()->findAll();
 
-		$posts = $this->EntityManager->getRepository(Posts::getClassName());
+		$posts                 = $this->EntityManager->getRepository(Posts::getClassName());
 		$this->template->posts = $posts->findAll();
 
 		$this->template->myparametr = $this->context->parameters['first_parametr'];
 
-		$this->template->test = $this->doSomeRefactoring('Hello world from blog');
+//		$this->template->test = $this->doSomeRefactoring('Hello world from blog');
 
 		//		$post = new Posts();
 //		$post->title = 'New title';
@@ -59,13 +61,38 @@ class BlogPresenter extends BasePresenter
 //		$dao->__set('title', 'test');
 
 
-	}
 
-	public function doSomeRefactoring($name){
+		try {
+			$this->checkNum(2);
+			\Tracy\Debugger::barDump('If you see this, the number is 1 or below');
 
-		if($name == '' . self::refactoringCondition . ''){
-
-	    echo $name;
+		} catch (Nette\Application\BadRequestException $e) {
+			Debugger::log('Message: ' . $e->getMessage());
+			var_dump($e->getMessage());
 		}
+//		finally {
+//			\Tracy\Debugger::barDump('Got here Finally');
+//		}
+
 	}
+
+	public function checkNum($number)
+	{
+		if ($number > 1) {
+
+			dump('cool before throw');
+			throw new Nette\Application\BadRequestException("Value must be 1 or below");
+		}
+//		dump('cool');
+		return FALSE;
+	}
+
+//	public function doSomeRefactoring($name)
+//	{
+//
+//		if ($name == '' . self::refactoringCondition . '') {
+//
+//			echo $name;
+//		}
+//	}
 }
